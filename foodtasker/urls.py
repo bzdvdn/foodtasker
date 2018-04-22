afec78062653424a9a3468a8de95dc391af1ddbc
bzdvdn <bzdv.dn@gmail.com>
2018-04-22 13:02:58 +0300

    Deploy

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -15,7 +15,24 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from foodtaskerapp import views as foodtasker_app_views
+from django.contrib.auth import views as auth_views
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', foodtasker_app_views.home, name='home'),
+    url(r'^restaurant/sign-in/$', auth_views.login,
+    	 {'template_name': 'restaurant/sign_in.html'
+    	 },
+    	 name='restaurant-sign-in'),
+    url(r'^restaurant/sign-out/$', auth_views.logout,
+    	{'next_page': '/'
+    	},name='restaurant-sign-out'),
+	 url(r'^restaurant/sign-up/$', foodtasker_app_views.restaurant_sign_up,
+    	name='restaurant-sign-up'),   
+    url(r'^restaurant/$', foodtasker_app_views.restaurant_home, name='restaurant-home')
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
